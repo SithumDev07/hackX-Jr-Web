81ac44a8de04cb2bf440ef4fbad1e1c72f6698c8
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-05-21 14:34:46 +0530

    responsive hero image

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        'mascotty-main' : "url('/public/images/Mas/mascotty.png')",
+      })
+    },
     screens: {
       sm: '640px',
       md: '768px',
