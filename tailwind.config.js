ac1659f3766cb31bb6348e37dd56c985760bb750
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-06-06 00:00:01 +0530

    finalized second section

--- tailwind.config.js
+++ tailwind.config.js
@@ -217,6 +217,7 @@ module.exports = {
       lg: ['0 10px 8px rgba(0, 0, 0, 0.04)', '0 4px 3px rgba(0, 0, 0, 0.1)'],
       xl: ['0 20px 13px rgba(0, 0, 0, 0.03)', '0 8px 5px rgba(0, 0, 0, 0.08)'],
       '2xl': '0 25px 25px rgba(0, 0, 0, 0.15)',
+      '3xl': '0 25px 25px rgba(59,136,246,50)',
       none: '0 0 #0000',
     },
     fill: { current: 'currentColor' },
